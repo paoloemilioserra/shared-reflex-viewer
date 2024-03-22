d980744288c896162e722cb6c86ea264741c3d6a
paoloemilioserra <paolo.serra@autodesk.com>
2024-03-22 11:49:45 +0100

    Update shared_reflex_viewer.py

--- shared_reflex_viewer/shared_reflex_viewer.py
+++ shared_reflex_viewer/shared_reflex_viewer.py
@@ -13,9 +13,9 @@
 # implied.  See the License for the specific language governing
 # permissions and limitations under the License.
 
-__author__ = 'Paolo Emilio Serra - paolo.serra@autodesk.com'
-__copyright__ = '2024'
-__version__ = '1.0.0'
+__author__ = "Paolo Emilio Serra - paolo.serra@autodesk.com"
+__copyright__ = "2024"
+__version__ = "1.0.0"
 
 
 import reflex as rx
@@ -23,12 +23,14 @@ import urllib
 import aps
 from shared_reflex_viewer import styles
 
+from shared_reflex_viewer.document_viewer import viewer
+
 
 token = aps.token
 
 
 class State(rx.State):
-    aps_token: str = rx.LocalStorage('{}', name='aps_token')
+    aps_token: str = rx.LocalStorage("{}", name="aps_token")
 
     def login(self):
         global token
@@ -37,17 +39,17 @@ class State(rx.State):
         parsed = urllib.parse.urlparse(fp)
         if len(parsed.query) == 0:
             return
-        code = urllib.parse.parse_qs(parsed.query).get('code', [''])[0]
+        code = urllib.parse.parse_qs(parsed.query).get("code", [""])[0]
         if not aps.is_token_valid():
             yield rx.remove_local_storage('aps_token')
-            token = aps.get_3_legged_token(('data:read', 'data:write', 'data:create'), code)
+            token = aps.get_3_legged_token(("data:read", "data:write", "data:create"), code)
         self.aps_token = repr(token)
 
 
-def create_viewer(urn: str) -> rx.Component:
+def create_viewer_old(urn: str) -> rx.Component:
     global token
     return rx.script(
-        '''
+        """
         var viewer;
         var md_ViewerDocument;
         var md_viewables;
@@ -57,12 +59,12 @@ def create_viewer(urn: str) -> rx.Component:
             var options = {
                 env: 'AutodeskProduction2',
                 api: 'streamingV2',
-                getAccessToken: function(onTokenReady) {                    
+                getAccessToken: function(onTokenReady) {
                     onTokenReady("%s", %s);
                 }
             }
 
-            <!-- This is called when the page is loaded--> 
+            <!-- This is called when the page is loaded-->
             Autodesk.Viewing.Initializer(options, function() {
 
                 var htmlDiv = document.getElementById('apsViewer');
@@ -81,7 +83,7 @@ def create_viewer(urn: str) -> rx.Component:
             var documentId = 'urn:' + '%s'; // Add the string 'urn:' to the actual URN value
             Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
         };
-    
+
         function onDocumentLoadSuccess(viewerDocument) {
 
             var viewerapp = viewerDocument.getRoot();
@@ -93,15 +95,30 @@ def create_viewer(urn: str) -> rx.Component:
                 console.error('Document contains no viewables.');
                 return;
             }
-            
+
             viewer.loadDocumentNode(viewerDocument, md_viewables[0]);
         }
 
         function onDocumentLoadFailure() {
             console.error('Failed fetching manifest');
         }
-        ''' % (token.Access, str(token.Expires), urn),
-        on_ready=rx.call_script('DisplayViewer();')
+        """
+        % (token.Access, str(token.Expires), urn),
+        on_ready=rx.call_script("DisplayViewer();"),
+     )
+
+
+def create_viewer(urn: str) -> rx.Component:
+    global token
+    if token.Access is None:
+        return rx.fragment()
+    return viewer(
+        name='apsViewer',
+        access_token=token.Access,
+        expires=str(token.Expires),
+        urn=urn,
+        width="100%",
+        height="600px",
     )
 
 
@@ -146,34 +163,24 @@ def get_style_sheet() -> rx.Component:
 def index() -> rx.Component:
     global token
     return rx.chakra.vstack(
-        rx.script(src="https://developer.api.autodesk.com/viewingservice/v2/viewers/three.min.js"),
-        rx.script(src="https://developer.api.autodesk.com/derivativeservice/v2/viewers/7.*/viewer3D.min.js"),
+        rx.script(
+            src="https://developer.api.autodesk.com/viewingservice/v2/viewers/three.min.js",
+            strategy="beforeInteractive",
+        ),
+        rx.script(
+            src="https://developer.api.autodesk.com/derivativeservice/v2/viewers/7.*/viewer3D.min.js",
+            strategy="beforeInteractive",
+        ),
         get_style_sheet(),
         rx.fragment(
             rx.chakra.heading("APS Viewer", font_size="2em"),
             menu_button(),
         ),
-        rx.chakra.box(
-            rx.chakra.box(
-                rx.chakra.vstack(
-                    rx.chakra.box(
-                        id='apsViewer',
-                        width='800px',
-                        height='600px',
-                        position='relative',  # the viewer is set by default as 'absolute' and it will not stay in its own div
-                        border='1em',
-                        color=styles.dark_slate,
-                    ),
-                ),
-
-            )
-        ),
-        create_viewer('dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLm1xNV9mVlJGUjV1SU1Cd29sUHNNLXc_dmVyc2lvbj0x'),  # Edit this URN to test the viewer with content you have access to
+        create_viewer("dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLm1xNV9mVlJGUjV1SU1Cd29sUHNNLXc_dmVyc2lvbj0x"),  # TODO change the urn to a model you have access to on Autodesk Docs
         on_mount=State.login,
     )
 
 
 # Create app instance and add index page.
-app = rx.App()  # stylesheets=['viewer.css'])
+app = rx.App()
 app.add_page(index, route='/', description='Autodesk Consulting', title='APS Viewer')
-
